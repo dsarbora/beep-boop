0c29a9aa540a3c7b4e81e62c1f3272e66521225b
Dave Sarbora <dsarbora@gmail.com>
2019-01-25 13:11:36 -0800

    add images and styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
-var beep = [0]
-var boop = [1]
-
 var halFunction = function(number){
-  for(var i = 0; i <= number; i++){
-    var iStr = i.toString();
-    var iArray = iStr.split('');
-    if(i !== 0 && i % 3 === 0){
-      resultArray.push("I'm sorry Dave, I can't do that.")
-    }
-    else if(iArray.includes("1")){
-      resultArray.push("boop");
-    }
-    else if(iArray.includes("0")){
-      resultArray.push("beep");
-    }
-    else{
-      resultArray.push(i)
+  debugger;
+  if (number){
+    for(var i = 0; i <= number; i++){
+      var iStr = i.toString();
+      var iArray = iStr.split('');
+      if(i !== 0 && i % 3 === 0){
+        resultArray.push("I'm sorry Dave, I can't do that.")
+      }
+      else if(iArray.includes("1")){
+        resultArray.push("Boop!");
+      }
+      else if(iArray.includes("0")){
+        resultArray.push("Beep!");
+      }
+      else{
+        resultArray.push(i)
+      };
     };
+  }
+  else{
+    alert("I'm sorry, I can't do that John")
   };
+};
 
   // if(parseInt(number) % 3 === 0){
   //   return "I'm sorry Dave, I'm afraid I can't do that"
@@ -30,12 +34,12 @@ var halFunction = function(number){
   // });
   //
   // alert("Please enter a number");
-};
 
 
 $(function(){
   $("#artificialLifeForm").submit(function(event){
     resultArray = []
+    $("#subtitle").show();
     event.preventDefault();
     var number = parseInt($("#number").val());
     var result = halFunction(number);
